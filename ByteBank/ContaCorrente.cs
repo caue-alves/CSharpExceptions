596ce021f8818a6f55783e8c3c2a93cfb23ba60c
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-09 15:38:00 -0300

    Add files via upload

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,93 @@
+﻿// using _05_ByteBank;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double taxtaOperacao {get; private set;}
+
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            taxtaOperacao = 30 /TotalDeContasCriadas;
+
+            TotalDeContasCriadas++;
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
